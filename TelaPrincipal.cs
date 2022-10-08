4fcafc7bcfd8faf5e92bac7fe1d84023aaf5f738
Hissamura <renanhissamura@hotmail.com>
2022-10-08 18:56:04 -0300

    Inserido imagem de fundo e campos de login.

---
+++ TelaPrincipal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_CadastroConsulta_RelatorioFinanceiro
+{
+    public partial class TelaPrincipal : Form
+    {
+        private TextBox txtLoginUsuario;
+        private Label lblSenhaUsuario;
+        private TextBox txtSenhaUsuario;
+        private Button btnEntrar;
+        private Label lblLogin;
+
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtLoginUsuario = new System.Windows.Forms.TextBox();
+            this.lblSenhaUsuario = new System.Windows.Forms.Label();
+            this.txtSenhaUsuario = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(187, 60);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(33, 13);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login";
+            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLoginUsuario
+            // 
+            this.txtLoginUsuario.Location = new System.Drawing.Point(128, 76);
+            this.txtLoginUsuario.Name = "txtLoginUsuario";
+            this.txtLoginUsuario.Size = new System.Drawing.Size(150, 20);
+            this.txtLoginUsuario.TabIndex = 1;
+            // 
+            // lblSenhaUsuario
+            // 
+            this.lblSenhaUsuario.AutoSize = true;
+            this.lblSenhaUsuario.Location = new System.Drawing.Point(187, 99);
+            this.lblSenhaUsuario.Name = "lblSenhaUsuario";
+            this.lblSenhaUsuario.Size = new System.Drawing.Size(38, 13);
+            this.lblSenhaUsuario.TabIndex = 2;
+            this.lblSenhaUsuario.Text = "Senha";
+            this.lblSenhaUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSenhaUsuario
+            // 
+            this.txtSenhaUsuario.Location = new System.Drawing.Point(128, 115);
+            this.txtSenhaUsuario.Name = "txtSenhaUsuario";
+            this.txtSenhaUsuario.Size = new System.Drawing.Size(150, 20);
+            this.txtSenhaUsuario.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(168, 141);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // TelaPrincipal
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(411, 277);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txtSenhaUsuario);
+            this.Controls.Add(this.lblSenhaUsuario);
+            this.Controls.Add(this.txtLoginUsuario);
+            this.Controls.Add(this.lblLogin);
+            this.DoubleBuffered = true;
+            this.Name = "TelaPrincipal";
+            this.Opacity = 0.5D;
+            this.Text = "Consulta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
