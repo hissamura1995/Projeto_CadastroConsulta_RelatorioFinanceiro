1b359292ab83f29f8a1bbaa85e2d61ca5a4b0d47
renan <renan@DESKTOP-47HSNSI>
2022-10-08 16:39:46 -0300

    Adicionar arquivos de projeto.

---
+++ Business/Model/ClienteModel.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+
+namespace Projeto_CadastroConsulta_RelatorioFinanceiro.Business.Model
+{
+    [Serializable]
+    public class ClienteModel
+    {
+        /// <summary>
+        /// Armazena IdUsuario
+        /// </summary>
+        public int IdUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena LoginUsuario
+        /// </summary>
+        public string LoginUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena SenhaUsuario
+        /// </summary>
+        public string SenhaUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena EmailUsuario
+        /// </summary>
+        public string EmailUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena CPFUsuario
+        /// </summary>
+        public int CPFUusuario { get; set; }
+
+        /// <summary>
+        /// Armazena DddUsuario
+        /// </summary>
+        public string DddUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena TelefoneUsuario
+        /// </summary>
+        public string TelefoneUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena EnderecoUsuario
+        /// </summary>
+        public string EnderecoUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena EstadoUsuario
+        /// </summary>
+        public string EstadoUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena CidadeUsiario
+        /// </summary>
+        public string CidadeUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena SexoUsuario
+        /// </summary>
+        public string SexoUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena NomeCompletoUsuario
+        /// </summary>
+        public string NomeCompletoUsuario { get; set; }
+
+        /// <summary>
+        /// Armazena EstadoCivilUsuario
+        /// </summary>
+        public string EstadoCivilUsuario { get; set; }
+
+    }
+}
